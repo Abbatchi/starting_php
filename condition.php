083df8c23e8bec3ef1cc7a464a55ae2408431cae
Vladislav Legoshin <abbatchi@gmail.com>
2017-03-07 20:21:49 +0100

    corrige 4

--- condition.php
+++ condition.php
@@ -55,9 +55,8 @@ echo "<h1 class='rainbow'>Exo3 <br> </h1>";
 echo "<h1 class='rainbow'>Exo4 <br> </h1>";
 
 	$magnitude = 5;
-		$tableau = ["Micro-séisme impossible à ressentir.", "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.". "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.", "Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.", "Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.", "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.", "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.", "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.", "Séisme capable de tout détruire sur une très vaste zone."];
-		echo $tableau[$magnitude -1];
-
+		$tableau = ["Micro-séisme impossible à ressentir.", "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.", "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.", "Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.", "Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.", "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.", "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.", "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.", "Séisme capable de tout détruire sur une très vaste zone."];
+		echo $tableau[$magnitude - 1];
 
 
 
