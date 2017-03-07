8b2b63876f71b21247df375ceb21e302194ef2c3
Vladislav Legoshin <abbatchi@gmail.com>
2017-03-07 16:18:06 +0100

    4

--- variables.php
+++ variables.php
@@ -1,15 +1,40 @@
 <?php 
 
+	$name = "Hesperus";
+
+	$name2 = "Hesperus";
+	$surname = "Lit";
+	$age = 33;
+
+	$kilo = 1;
+
+	$string = "String";
+	$int = 333;
+	$float = 1.3;
+	$boolean = "true";
+
+	$int2 = (int)NULL;
+
+	$name4 = "Alex";
+
+	$name3 = "BOB";
+	$surname3 = "Kevin";
+	$age3 = 100;
+
+	$var1 = 3 + 4;
+	$var2 = 5 * 20;
+	$var3 = 45 / 5;
+
+	
+
 echo "<h1 class='rainbow'>Exo1 <br> </h1>";
 
-	$name = "<p>Hesperus</p>";
+
 		echo $name. "<br>";
 
 echo "<h1 class='rainbow'>Exo2 <br> </h1>";
 
-	$name2 = "<p>Hesperus</p>";
-	$surname = "<p>Lit</p>";
-	$age = 33;
+
 		echo $name2;
 		echo $surname. "<br>";
 		echo $age. "<br>";
@@ -17,7 +42,7 @@ echo "<h1 class='rainbow'>Exo2 <br> </h1>";
 
 echo "<h1 class='rainbow'>Exo3 <br> </h1>";
 
-	$kilo = 1;
+
 		echo $kilo. "<br>";
 	$kilo = 3;
 		echo $kilo. "<br>";
@@ -27,10 +52,7 @@ echo "<h1 class='rainbow'>Exo3 <br> </h1>";
 
 echo "<h1 class='rainbow'>Exo4 <br> </h1>";
 
-	$string = "<p>String</p>";
-	$int = 333;
-	$float = 1.3;
-	$boolean = "true";
+
 
 		echo $string. "<br>";
 		echo $int. "<br>";
@@ -40,30 +62,26 @@ echo "<h1 class='rainbow'>Exo4 <br> </h1>";
 
 echo "<h1 class='rainbow'>Exo5 <br> </h1>";
 	
-	$int2 = (int)NULL;
+
 		echo $int2. "<br>";
 	$int2 = 33;
 		echo $int2. "<br>";
 
 echo "<h1 class='rainbow'>Exo6 <br> </h1>";
 
-	$name = "Alex";
-		echo "Bonjour ".$name .", comment vas tu?";
+
+		echo "Bonjour ".$name4 .", comment vas tu?";
 
 
 echo "<h1 class='rainbow'>Exo7 <br> </h1>";
 
-	$name3 = "BOB";
-	$surname3 = "Kevin";
-	$age3 = 100;
+
 
 		echo "Bonjour ".$name3.", tu t`apelles ".$surname3." et tu as ".$age3." ans!";
 
 echo "<h1 class='rainbow'>Exo8 <br> </h1>";
 
-	$var1 = 3 + 4;
-	$var2 = 5 * 20;
-	$var3 = 45 / 5;
+
 
 	echo $var1."<br>".$var2."<br>".$var3;
 
