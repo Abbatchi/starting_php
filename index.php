8fad1324454d22517de8a143f70f02e759106aae
Vladislav Legoshin <abbatchi@gmail.com>
2017-03-07 14:04:25 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exos PHP</title>
+</head>
+<body>
+	<a href="variables.php">Exos</a>
+
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+
+$name = "Hesperus";
+echo $name. "<br>";
+echo "<br><br>";
+
+
+
+$surname = "Lit";
+$age = 33;
+
+
+	echo $name;
+	echo $surname. "<br>";
+	echo $age. "<br>";
+
+	echo "<br><br>";
+
+$kilo = 1;
+
+echo $kilo. "<br>";
+
+$kilo = 3;
+
+echo $kilo. "<br>";
+
+$kilo = 125;
+
+echo $kilo. "<br>";
+
+?>
