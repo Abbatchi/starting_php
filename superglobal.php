a0d6bf29d19950c53a692f297ded018c93e1c143
Vladislav Legoshin <abbatchi@gmail.com>
2017-03-09 11:02:49 +0100

    cookie

---
+++ superglobal.php
@@ -0,0 +1,60 @@
+<?php 
+	
+	echo "<h1 class='rainbow'>Exo1 <br> </h1>";	
+
+	echo "Server ip: ".$_SERVER["SERVER_ADDR"];
+	echo "<br>";
+	echo "User agent: ".$_SERVER["HTTP_USER_AGENT"];
+	echo "<br>";
+	echo "Server host: ".$_SERVER["HTTP_HOST"];
+	echo "<br>";
+	echo "User ip: ".$_SERVER["REMOTE_ADDR"];
+
+	echo "<h1 class='rainbow'>Exo2 <br> </h1>";	
+
+
+
+		session_start();
+
+
+		$_SESSION['nom'] = 'Formateur';
+		$_SESSION['prenom']   = 'Julien';
+		$_SESSION['age']     = 100;
+
+		echo '<br /><a href="superglobal2.php">page2</a>';
+
+
+	echo "<h1 class='rainbow'>Exo2 <br> </h1>";	
+
+if (isset($_GET['login'])) {
+	
+	$login = $_GET['login'];
+	$pass = $_GET['pass'];
+
+			setcookie("login", $login, time()+3600);
+			setcookie("pass", $pass, time()+3600);
+			header('Location:superglobal2.php');
+}
+
+ ?>
+
+
+
+<!DOCUMENT HTML>
+<html>
+		<head>
+			<title>Exos PHP</title>
+			<link rel="stylesheet" href="style/css/style.css">
+		</head>
+	<body>
+
+		<form action="superglobal.php" method="get">
+		<p>
+			<p>Login <input type="text" placeholder="LOGIN" name="login" /></p>
+		    <p>Password <input type="text" placeholder="PASSWORD" name="pass" /></p>
+		    <input type="submit" value="Valider" />
+		</p>
+		</form>
+		
+	</body>
+</html>
---
+++ superglobal2.php
@@ -0,0 +1,45 @@
+<?php 
+session_start();
+
+echo 'Bienvenue sur la page numéro 2<br />';
+
+echo $_SESSION['nom'];
+echo $_SESSION['prenom'];
+echo $_SESSION['age'];
+
+
+echo '<br /><a href="superglobal.php">page 1</a>';
+
+	
+
+	echo "<br />votre cookie login est: ".$_COOKIE['login']."<br>";
+	echo "votre cookie pass est: ".$_COOKIE['pass'];
+
+echo '<br /><br />modifier<br /><br />';
+
+
+ ?>
+
+
+
+
+
+
+
+<!DOCUMENT HTML>
+<html>
+		<head>
+			<title>Exos PHP</title>
+			<link rel="stylesheet" href="style/css/style.css">
+		</head>
+	<body>
+
+		<form action="superglobal.php" method="get">
+			<p>
+				<p>Login <input type="text" placeholder="LOGIN" name="login" /></p>
+			    <p>Password <input type="text" placeholder="PASSWORD" name="pass" /></p>
+			    <input type="submit" value="Valider" />
+			</p>
+		</form>
+	</body>
+</html>
